1643601ac49a525878e73145272764b820f51124
Protero <protero@outlook.es>
2021-09-09 20:36:46 +0200

    Create: index APP with router

--- exercise-advanced-pokedex/src/index.js
+++ exercise-advanced-pokedex/src/index.js
@@ -1,9 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+
+import Home from 'pages/home';
+import Details from 'pages/details';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* Your foundation */}
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/details" component={Details} />
+        <Redirect to="/" />
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
-)
+);
